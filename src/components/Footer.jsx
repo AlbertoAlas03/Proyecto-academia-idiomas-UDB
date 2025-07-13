dfd0b9a9de48f6508bc1fbea4f945cb5c266aee8
Alberto Alas <oa3973392@gmail.com>
2025-07-12 22:55:45 -0600

    Interface and login created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap"
+
+const Footer = () => {
+    return (
+        <footer className="footer fixed-bottom mt-auto py-3 bg-body-tertiary">
+            <Container>
+                <span className="text-body-secondary">© 2025 Academia de idiomas UDB</span>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
