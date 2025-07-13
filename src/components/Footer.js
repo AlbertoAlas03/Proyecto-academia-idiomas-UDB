ea8dc9a968671887b82b77d3b93ad7a2f731ae9e
Alberto Alas <oa3973392@gmail.com>
2025-07-12 22:13:57 -0600

    Sticky footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
         <>
 
             <div className="container">
-                <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <footer className="d-flex flex-wrap justify-content-between align-items-center my-4 border-top">
                     <p className="col-md-4 mb-0 text-body-secondary">© 2025 Academia de idiomas UDB</p>
                 </footer>
             </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import logo from "../assets/img/logo.png"
 import useLogin from "../hooks/use-login";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/auth-context";
+import Footer from "./Footer"
 
 const Login = () => {
 
@@ -66,59 +67,64 @@ const Login = () => {
     }, [token, navigate]);
 
     return (
-        <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-            <div className="card shadow-lg rounded-3 w-75" style={{ maxWidth: '500px' }}>
-                <div className="card-body">
-                    <h2 className="text-center mb-4" style={{ fontSize: '2rem' }}>Iniciar Sesión</h2>
-                    <img src={logo} alt="Usuario" className="img-fluid mx-auto d-block mb-4" style={{ width: '220px', height: '100px' }} />
-                    {error && (
-                        <div className="alert alert-danger d-flex align-items-center" role="alert">
-                            <button className="me-2" style={{ background: 'transparent', border: 'none' }} onClick={() => setError(null)}>
-                                <i className="bi bi-x-circle-fill"></i>
-                            </button>
-                            {error}
-                        </div>
-                    )}
-                    {loading && (
-                        <div className="alert alert-info d-flex align-items-center" role="alert">
-                            <i className="bi bi-hourglass-split me-2"></i>
-                            Iniciando sesión...
-                        </div>
-                    )}
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-4">
-                            <label htmlFor="email" className="form-label" style={{ fontSize: '1.1rem' }}>Correo electrónico</label>
-                            <input type="email" className="form-control form-control-lg" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Ingrese su correo electrónico" style={{ fontSize: '1.1rem' }} disabled={loading}
-                            />
-                        </div>
-
-                        <div className="mb-4">
-                            <label htmlFor="password" className="form-label" style={{ fontSize: '1.1rem' }}>Contraseña</label>
-                            <div className="input-group">
-                                <input type={showPassword ? 'text' : 'password'} className="form-control form-control-lg" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Ingrese su contraseña" style={{ fontSize: '1.1rem' }} disabled={loading}
-                                />
-                                <button className="input-group-text" onClick={togglePasswordVisibility}><i className={showPassword ? 'bi bi-eye-slash' : 'bi bi-eye'}></i></button>
+        <>
+            <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+                <div className="card shadow-lg rounded-3 w-75" style={{ maxWidth: '500px' }}>
+                    <div className="card-body">
+                        <h2 className="text-center mb-4" style={{ fontSize: '2rem' }}>Iniciar Sesión</h2>
+                        <img src={logo} alt="Usuario" className="img-fluid mx-auto d-block mb-4" style={{ width: '220px', height: '100px' }} />
+                        {error && (
+                            <div className="alert alert-danger d-flex align-items-center" role="alert">
+                                <button className="me-2" style={{ background: 'transparent', border: 'none' }} onClick={() => setError(null)}>
+                                    <i className="bi bi-x-circle-fill"></i>
+                                </button>
+                                {error}
                             </div>
-                        </div>
+                        )}
+                        {loading && (
+                            <div className="alert alert-info d-flex align-items-center" role="alert">
+                                <i className="bi bi-hourglass-split me-2"></i>
+                                Iniciando sesión...
+                            </div>
+                        )}
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-4">
+                                <label htmlFor="email" className="form-label" style={{ fontSize: '1.1rem' }}>Correo electrónico</label>
+                                <input type="email" className="form-control form-control-lg" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Ingrese su correo electrónico" style={{ fontSize: '1.1rem' }} disabled={loading}
+                                />
+                            </div>
 
-                        <div className="text-center mb-4">
-                            <button type="submit" className="btn btn-primary w-100 btn-lg">
-                                {loading ? (
-                                    <>
-                                        <i className="bi bi-arrow-repeat me-2 spin"></i> Iniciado sesión...
-                                    </>
-                                ) : (
-                                    <>
-                                        <i className="bi bi-box-arrow-in-right me-2"></i>
-                                        Iniciar Sesión
-                                    </>
-                                )}
-                            </button>
-                        </div>
-                    </form>
-                </div>
+                            <div className="mb-4">
+                                <label htmlFor="password" className="form-label" style={{ fontSize: '1.1rem' }}>Contraseña</label>
+                                <div className="input-group">
+                                    <input type={showPassword ? 'text' : 'password'} className="form-control form-control-lg" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Ingrese su contraseña" style={{ fontSize: '1.1rem' }} disabled={loading}
+                                    />
+                                    <button className="input-group-text" onClick={togglePasswordVisibility}><i className={showPassword ? 'bi bi-eye-slash' : 'bi bi-eye'}></i></button>
+                                </div>
+                            </div>
+
+                            <div className="text-center mb-4">
+                                <button type="submit" className="btn btn-primary w-100 btn-lg">
+                                    {loading ? (
+                                        <>
+                                            <i className="bi bi-arrow-repeat me-2 spin"></i> Iniciado sesión...
+                                        </>
+                                    ) : (
+                                        <>
+                                            <i className="bi bi-box-arrow-in-right me-2"></i>
+                                            Iniciar Sesión
+                                        </>
+                                    )}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div >
             </div >
-        </div >
+            <div className="footer fixed-bottom">
+                <Footer />
+            </div>
+        </>
     )
 }
 
