0f6d25b6b8fac858de9d98fdeafbda669b1a40ff
Alberto Alas <oa3973392@gmail.com>
2025-08-19 00:40:57 -0600

    Interface login created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import { Container, Image } from "react-bootstrap"
+import logo from '../assets/img/logo.png'
+
+const Footer = () => {
+    return (
+        <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+            <Container className="col mb-3 text-center">
+                <Image src={logo} className="d-flex align-items-center mb-3 text-decoration-none" width='250px' height='100px' />
+                <p><strong>Copyright © 2025 - Academia de Idiomas UDB</strong></p>
+            </Container>
+            <Container className="col mb-3">
+                <h5>INFORMACIÓN</h5>
+                <ul className="nav flex-column">
+                    <li className="nav-item mb-2"><a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.udb.edu.sv%2Fudb%2Fpagina%2Fescidiomas&h=AT2KO4ssB-snw1UFklquz8xaOcfExXOT9JFnZ-79b62mzAtSecDncPx57Kz7GiyKJt2eyMNJWwITYV06mj5zhZq08wSDZ7WkIcphmwTbPs4coaWMTw3wqS6gYMB6M02A5f_4_45kK0H6EeAIHsE7" target="_blank">Página oficial - idiomas UDB</a></li>
+                </ul>
+            </Container>
+            <Container className="col mb-3">
+                <h5>CONTÁCTANOS</h5>
+                <ul className="nav flex-column">
+                    <li className="nav-item mb-2"><strong>Campus UDB Soyapango</strong></li>
+                    <li className="nav-item mb-2"><i className="bi bi-telephone"></i> Teléfono: (503) 2251-8219</li>
+                    <li className="nav-item mb-2"><strong>Campus UDB Antiguo Cuscatlán</strong></li>
+                    <li className="nav-item mb-2"><i className="bi bi-telephone"></i> Teléfono: (503) 2527-2324</li>
+                    <li className="nav-item mb-2"><strong>Nuestro E-mail:</strong></li>
+                    <li className="nav-item mb-2"><i className="bi bi-envelope"></i> <u>idiomas@udb.edu.sv</u></li>
+                    <li className="nav-item mb-2"><i className="bi bi-envelope"></i> <u>sabatinos@udb.edu.sv</u></li>
+                </ul>
+            </Container>
+            <Container className="col mb-3">
+                <h5>REDES SOCIALES</h5>
+                <ul className="nav flex-column">
+                    <li className="nav-item mb-2">
+                        <a href="https://www.facebook.com/escueladeidiomas.udb/?locale=es_LA" target="_blank">
+                            <i className="bi bi-facebook" style={{ fontSize: '50px' }}></i>
+                        </a>
+                    </li>
+                </ul>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/pages/Login.jsx
@@ -0,0 +1,105 @@
+import { Container, Card, Image, Form, Button, InputGroup } from "react-bootstrap"
+import logo from '../assets/img/logo.png'
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import fondo from '../assets/img/background_login.jpg'
+import Footer from "../components/Footer"
+
+const Login = () => {
+
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [seePassword, setSeePassword] = useState(false)
+
+    const handleSeePassword = () => {
+        setSeePassword(!seePassword)
+    }
+
+    return (
+        <>
+            <Container fluid style={{
+                backgroundImage: `url(${fondo})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                height: "100vh"
+            }}>
+                <Container className="container d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+                    <Card className="rounded-3 w-75" style={{ maxWidth: '500px' }}>
+                        <Card.Body>
+                            <Image src={logo} className="img-fluid mx-auto d-block mb-4" style={{ width: '220px', height: '100px' }} />
+                            {
+                                loading ? (
+                                    <div className="alert alert-info d-flex align-items-center" role="alert">
+                                        <i className="bi bi-hourglass-split me-2"></i>
+                                        Iniciando sesión...
+                                    </div>
+                                ) : (
+                                    error ? (
+                                        <Container className="alert alert-danger d-flex align-items-center" role="alert">
+                                            <button className="me-2" style={{ background: 'transparent', border: 'none' }} onClick={() => setError(null)}>
+                                                <i className="bi bi-x-circle-fill"></i>
+                                            </button>
+                                            <span>{error}</span>
+                                        </Container>
+                                    ) : (
+                                        <Container className="alert alert-success d-flex align-items-center" role="alert">
+                                            <span>Puedes iniciar sesión utilizando las credenciales que se le han proporcionado. <strong>Si aún no cuenta con credenciales de acceso, por favor comuníquese con el área administrativa de la Escuela de Idiomas para solicitarlas.</strong></span>
+                                        </Container>
+                                    ))
+
+                            }
+                            <Form onSubmit={{}}>
+                                <fieldset disabled={loading}>
+                                    <Form.Group className="mb-4">
+                                        <Form.Label><i className="bi bi-envelope-at"></i> Correo eléctronico</Form.Label>
+                                        <Form.Control
+                                            type="email"
+                                            placeholder="Correo eléctronico"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-4">
+                                        <Form.Label><i className="bi bi-lock"></i> Contraseña</Form.Label>
+                                        <InputGroup>
+                                            <Form.Control
+                                                type={seePassword ? 'text' : 'password'}
+                                                placeholder="Contraseña"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                            />
+                                            <Button variant="outline-success" onClick={(e) => handleSeePassword(e)}>
+                                                {
+                                                    seePassword ? (
+                                                        <i className="bi bi-eye-slash"></i>
+                                                    ) : (
+                                                        <i className="bi bi-eye"></i>
+                                                    )
+                                                }
+
+                                            </Button>
+                                        </InputGroup>
+
+                                    </Form.Group>
+                                    <Container>
+                                        <i className="bi bi-key"></i>
+                                        <Link to='/prueba'>¿Olvidó su contraseña?</Link>
+                                    </Container>
+                                    <hr />
+                                    <Button className="btn btn-primary w-100" type="submit">Iniciar sesión</Button>
+                                </fieldset>
+                            </Form>
+                        </Card.Body >
+                    </Card >
+                </Container >
+                <Footer />
+            </Container>
+        </>
+    )
+}
+
+export default Login
